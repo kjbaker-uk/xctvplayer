f68b3aa02585a4650f976f2dd198309443261ff2
Keith <d4rkc00d3r@gmail.com>
2021-12-25 11:57:05 +0000

    🧑‍💻Initial code commit

---
+++ app/src/main/java/com/pixeladdict/xctvplayer/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.pixeladdict.xctvplayer
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    override fun onBackPressed() {
+
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply{
+            // We need to set a userAgent to trick the service into thinking we are a legit device.
+            settings.userAgentString = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            loadUrl(DEFAULT_URL)
+        }
+    }
+
+    inner class WebViewClient: android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+        }
+    }
+
+
+    companion object {
+        private const val DEFAULT_URL = "https://www.xbox.com/en-Us/play"
+    }
+}
