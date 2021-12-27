840b3daef00b2a9146ecb3d66c1aa77dbbc504e1
Keith <d4rkc00d3r@gmail.com>
2021-12-27 16:02:09 +0000

    V1.1.0 Update
    https://github.com/D4rkC00d3r/xctvplayer/blob/main/README.md#changelog

--- app/src/main/java/com/pixeladdict/xctvplayer/MainActivity.kt
+++ app/src/main/java/com/pixeladdict/xctvplayer/MainActivity.kt
@@ -2,9 +2,15 @@ package com.pixeladdict.xctvplayer
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebSettings
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import android.webkit.CookieSyncManager
+
+import android.os.Build
+
 
 class MainActivity : AppCompatActivity() {
     private val webView: WebView by lazy {
@@ -14,13 +20,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         initViews()
     }
 
     override fun onBackPressed() {
 
-        if(webView.canGoBack()) {
+        if (webView.canGoBack()) {
             webView.goBack()
         } else {
             super.onBackPressed()
@@ -30,8 +35,11 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews() {
-        webView.apply{
+        webView.apply {
             // We need to set a userAgent to trick the service into thinking we are a legit device.
+            settings.setRenderPriority(WebSettings.RenderPriority.HIGH)
+            settings.cacheMode = WebSettings.LOAD_DEFAULT
+            settings.allowContentAccess = true
             settings.userAgentString = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
@@ -41,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    inner class WebViewClient: android.webkit.WebViewClient() {
+    inner class WebViewClient : android.webkit.WebViewClient() {
 
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
             super.onPageStarted(view, url, favicon)
@@ -52,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    inner class WebChromeClient: android.webkit.WebChromeClient() {
+    inner class WebChromeClient : android.webkit.WebChromeClient() {
         override fun onProgressChanged(view: WebView?, newProgress: Int) {
             super.onProgressChanged(view, newProgress)
         }
